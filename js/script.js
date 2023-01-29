c1ba8012a74bdb8b6f2009431cb57a73ea258a4b
codinghuntsman <sajedulislam.cse@gmail.com>
2023-01-29 20:31:54 +0600

    all code added

---
+++ js/script.js
@@ -0,0 +1,22 @@
+document.getElementById("click-btn").addEventListener("click", () => {
+  fetch("https://api.thedogapi.com/v1/breeds")
+    .then((res) => res.json())
+    .then((data) => ShowData(data));
+});
+
+const main = document.getElementById("main");
+const ShowData = (dogList) => {
+  const TenDogList = dogList.slice(0, 21);
+  console.log(TenDogList);
+  for (const dog of TenDogList) {
+    const div = document.createElement("div");
+    div.classList = "col col-lg-4";
+    div.innerHTML = `
+    <h4>${dog.name}</h4>
+    <p>${dog.bred_for}</p>
+    <p>${dog.breed_group}</p>
+    <img width="300px" height="200px" src="${dog.image.url}" />
+    `;
+    main.appendChild(div);
+  }
+};
